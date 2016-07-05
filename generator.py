648ef8f318fe73b748d986e4ae0d882a20e6edcb
deb <debkbanerji@gmail.com>
2016-07-05 11:29:23 -0400

    'generator.py'
    Created generator.py

---
+++ generator.py
@@ -0,0 +1,11 @@
+def create_main_directory():
+    print ("Creating project directory...")
+
+print ("""This generator will create the base of your
+Node.js + Express + Angular 1 project\n""")
+
+print ("""Note: when entering a module name or the project name,
+use words separated by spaces.
+Case conversion will take place automatically.""")
+
+project_name = raw_input ("\nEnter your project name\n")
